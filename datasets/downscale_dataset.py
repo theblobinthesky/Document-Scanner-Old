6e2e7b1c99d9894a4e79e130aa5bdfa445d86451
erik <erik.stern@outlook.de>
2023-03-14 23:52:02 +0100

    Increased amount of adverserial examples for segmentation model.

--- datasets/downscale_dataset.py
+++ datasets/downscale_dataset.py
@@ -13,10 +13,10 @@ import concurrent.futures
 out_size = (64, 64)
 
 dir_items = [
-    ("Doc3d/img/1", "Doc3d_64x64/img/1"), ("Doc3d/img/2", "Doc3d_64x64/img/2"), 
-    ("Doc3d/img/3", "Doc3d_64x64/img/3"), ("Doc3d/img/4", "Doc3d_64x64/img/4"),
-    ("Doc3d/lines/1", "Doc3d_64x64/lines/1"), ("Doc3d/lines/2", "Doc3d_64x64/lines/2"), 
-    ("Doc3d/lines/3", "Doc3d_64x64/lines/3"), ("Doc3d/lines/4", "Doc3d_64x64/lines/4"), 
+    # ("Doc3d/img/1", "Doc3d_64x64/img/1"), ("Doc3d/img/2", "Doc3d_64x64/img/2"), 
+    # ("Doc3d/img/3", "Doc3d_64x64/img/3"), ("Doc3d/img/4", "Doc3d_64x64/img/4"),
+    # ("Doc3d/lines/1", "Doc3d_64x64/lines/1"), ("Doc3d/lines/2", "Doc3d_64x64/lines/2"), 
+    # ("Doc3d/lines/3", "Doc3d_64x64/lines/3"), ("Doc3d/lines/4", "Doc3d_64x64/lines/4"), 
     ("MitIndoor", "MitIndoor_64x64"),
 ]
 
