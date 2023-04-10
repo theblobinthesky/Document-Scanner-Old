2089a39218d61b181f2226346a29f7a494ae56e6
erik <erik.stern@outlook.de>
2023-04-10 22:42:08 +0200

    3d tracking.

--- autolabel/autolabel.py
+++ autolabel/autolabel.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 from glob import glob
 import cv2
-import numpy as np
-import torch
-from seg_model import SegModel
-from model import device
-import pygame
 from tqdm import tqdm
 import time
+from mesher import solve_in_3d, test
 
 dir = "/media/shared/Projekte/DocumentScanner/datasets/Custom"
 model_path = "models/main_seg_2.pth"
@@ -21,350 +17,29 @@ track_box_size = 51
 hbs = track_box_size // 2
 iters = 4
 
+images_dir = "data/images/new"
+output_dir = "data/output"
 
-paths = ["/media/shared/Projekte/DocumentScanner/datasets/Custom/20230319_175025.mp4"] # 20230407_184309
-paths = glob(f"{dir}/*.mp4")
+print()
+print(f"Working on '{images_dir}'...")
 
-model = SegModel()
-model.load_state_dict(torch.load(model_path))
-model = model.to(device=device)
+start_time = time.time()
 
-pygame.init()
+paths = glob(f"{images_dir}/*.jpg")
 
-def visualize_track(start_index, track):
-    for i in range(len(track)):
-        if track[i] == None: break
-
-        fi = start_index + i
-        cap.set(cv2.CAP_PROP_POS_FRAMES, fi)
-        frame = read_frame(cap)
-
-        color = (255, 0, 0) if i == 0 else (0, 255, 0)
-        for box in track[i]:
-            if box == None: continue
-            x, y, w, h = box
-            x, y, w, h = int(x), int(y), int(w), int(h)
-            cx, cy = int(round(x + w / 2.0)), int(round(round(y + h / 2.0)))
-
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
-            cv2.circle(frame, (cx, cy), 5, color, -1)
-
-
-        surf = pygame.surfarray.make_surface(np.rot90(frame))
-        surf = pygame.transform.scale(surf, screen_size)
-
-        screen.blit(surf, (0, 0))
-        pygame.display.update()
-
-        # check for pygame events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                cap.release()
-                pygame.quit()
-                quit()
-
-
-def get_segmask_from_model(frame):
-    with torch.no_grad():
-        frame = frame.astype("float32") / 255.0
-        ten = torch.from_numpy(frame).to(device=device)
-        ten = ten.permute(2, 0, 1).unsqueeze(0)
-
-        segmask = model(ten)
-        segmask = segmask.squeeze(0).permute((1, 2, 0))
-    return segmask.detach().cpu().numpy()
-
-
-def read_frame(cap):
-    _, frame = cap.read()
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = cv2.resize(frame, (processing_size, processing_size))
-
-    return frame
-
-def get_largest_contour(mask):
-    contours, _  = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contour = max(contours, key=lambda contour: cv2.contourArea(contour))
-    return contour
-            
-def get_mask_from_contour(contour, size):
-    contour = contour.astype("int32")
-    mask = np.zeros(size, np.uint8)
-    mask = cv2.fillPoly(mask, [contour], (255))
-    return mask
-            
-def get_simple_contour(contour, fac=0.12):
-    return cv2.approxPolyDP(contour, fac * cv2.arcLength(contour, True), True)
-
-def isolate_largest_mask(mask):
-    contour = get_largest_contour(mask)
-    mask = get_mask_from_contour(contour, mask.shape[0:2])
-    return mask
-
-
-def unsharpen_mask(frame, sigma=2.0, alpha=2.0):
-    gaussian_3 = cv2.GaussianBlur(frame, (0, 0), sigma)
-    unsharp_image = cv2.addWeighted(frame, alpha, gaussian_3, -1.0, 0)
-    return unsharp_image
-
-def grab_cut(frame, pr_fg_mask, fg_mask, bg_mask, size):
-    fg_model, bg_model = np.zeros((1, 65), np.float64), np.zeros((1, 65), np.float64)
-            
-    mask = np.zeros(size, np.uint8)
-    mask[bg_mask == 0] = cv2.GC_PR_BGD
-    mask[bg_mask == 255] = cv2.GC_BGD
-    mask[pr_fg_mask == 255] = cv2.GC_PR_FGD
-    mask[fg_mask == 255] = cv2.GC_FGD
-    cv2.grabCut(frame, mask, None, bg_model, fg_model, 6, cv2.GC_INIT_WITH_MASK)       
-    mask = np.where((mask == cv2.GC_FGD) | (mask == cv2.GC_PR_FGD), 255, 0).astype('uint8')
-
-    return get_largest_contour(mask)
-
-def do_dense_segmentation(cap, frames):
-    contours_enum = []
-    
-    for i in tqdm(range(frames), desc="Dense segmentation"):
-        frame = read_frame(cap)
-        small_frame = cv2.resize(frame, (model_size, model_size))
-
-
-        pr_fg_mask = get_segmask_from_model(small_frame)
-        pr_fg_mask = (pr_fg_mask > binarize_threshold).astype("uint8") * 255
-        pr_fg_mask = isolate_largest_mask(pr_fg_mask)
-
-                
-        small_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (mask_size_kernels[0], mask_size_kernels[0]))
-        large_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (mask_size_kernels[1], mask_size_kernels[1]))
-        fg_mask, bg_mask = cv2.erode(pr_fg_mask, small_kernel), cv2.dilate(pr_fg_mask, large_kernel)        
-        bg_mask = cv2.bitwise_not(bg_mask)
-
-        # resize them to be of frame size, not small frame size
-        def upscale_small_mask(small):
-            large = cv2.resize(small, frame.shape[0:2])
-            return (large == 255).astype("uint8") * 255
-
-        pr_fg_mask, fg_mask, bg_mask = upscale_small_mask(pr_fg_mask), upscale_small_mask(fg_mask), upscale_small_mask(bg_mask)
-
-        # unshapen mask frame
-        frame = unsharpen_mask(frame, sigma=4.0, alpha=2.0)
-
-
-        # grabcut the fine contours
-        contour = grab_cut(frame, pr_fg_mask, fg_mask, bg_mask, frame.shape[0:2])
-        simple_contour = get_simple_contour(contour)
-        mask, simple_mask = get_mask_from_contour(contour, frame.shape[0:2]), get_mask_from_contour(simple_contour, frame.shape[0:2])
-
-        conj = cv2.bitwise_and(mask, cv2.bitwise_not(simple_mask))
-        ratio = conj.sum() / simple_mask.sum()
-        if len(simple_contour) != 4 or ratio > max_ratio:
-            continue
-
-        surf = pygame.surfarray.make_surface(np.rot90(cv2.bitwise_and(frame, simple_mask[:, :, np.newaxis].repeat(3, 2))))
-        surf = pygame.transform.scale(surf, screen_size)
-        screen.blit(surf, (0, 0))
-        pygame.display.update()
-            
-        simple_contour = simple_contour.reshape((-1, 2))
-        contours_enum.append((i, simple_contour))
-
-    return contours_enum
-
-def do_fused_track(cap, contours_enum):
-    tracks = []
-    trackers = [ cv2.TrackerCSRT_create() for _ in range(4) ]
-
-
-    def update_trackers_and_get_boxes(frame):
-        boxes = []
-        for i in range(4):
-            success, box = trackers[i].update(frame)
-        
-            if success: boxes.append(box)
-            else: boxes.append(None)
-            
-        return boxes
-
-
-    for i, (frame_index, simple_contour) in enumerate(tqdm(contours_enum, desc="Tracking")):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        frame = read_frame(cap)
-        
-        boxes = [(x - hbs, y - hbs, track_box_size, track_box_size) for (x, y) in simple_contour]
-        for t in range(4):
-            trackers[t].init(frame, boxes[t])
-            
-
-        # track forward
-        if i == len(contours_enum) - 1:
-            next_frame_index = frames - 1
-        else:
-            next_frame_index = contours_enum[i + 1][0]
-
-        fw_track = [boxes]
-        for fw_frame_index in range(frame_index + 1, next_frame_index + 1):
-            cap.set(cv2.CAP_PROP_POS_FRAMES, fw_frame_index)
-            fw_frame = read_frame(cap)
-            fw_track.append(update_trackers_and_get_boxes(frame))
-
-
-        # track backward
-        if i == 0:
-            last_frame_index = 0
-        else:
-            last_frame_index = contours_enum[i - 1][0]
-            
-        bw_track = [boxes]
-        for bw_frame_index in range(last_frame_index, frame_index).__reversed__():
-            cap.set(cv2.CAP_PROP_POS_FRAMES, bw_frame_index)
-            bw_frame = read_frame(cap)
-            bw_track.append(update_trackers_and_get_boxes(frame))
-            
-        bw_track.reverse()
-
-
-        tracks.append(((last_frame_index, frame_index, next_frame_index), fw_track, bw_track))
-
-
-    # fuse the forward and backward tracking
-    fused_track = []
-
-    def add_to_fused_track(fwt, bwt):
-        if fwt == None: fwt = bwt
-        if bwt == None: bwt = fwt
-
-        roll_errors = []
-        for roll in range(4):
-            error = 0.0
-            for i in range(len(fwt)):
-                f, b = np.array(fwt[i]), np.roll(np.array(bwt[i]), roll, axis=0)
-                error += np.abs(f - b).sum()
-
-            roll_errors.append(error)
-
-        roll = roll_errors.index(min(roll_errors))
-
-        for i in range(len(fwt) - 1):
-            t = i / float(len(fwt) - 1)
-            f, b = np.array(fwt[i]), np.roll(np.array(bwt[i]), roll, axis=0)
-            fused = b * t + f * (1.0 - t)
-            fused_track.append(fused.tolist())
-
-
-    if len(tracks) > 0:
-        _, _, c_bwt = tracks[0]
-        add_to_fused_track(None, c_bwt)
-    
-        for t in range(len(tracks) - 1):
-            _, c_fwt, _ = tracks[t]
-            _, _, n_bwt = tracks[t + 1]
-                
-            add_to_fused_track(c_fwt, n_bwt)
-    
-        _, c_fwt, _ = tracks[-1]
-        add_to_fused_track(c_fwt, None)
-    else:
-        print("error!")
-        exit()
-
-    return fused_track
-
-
+# solve_in_3d(images_dir, output_dir)
+test(images_dir, output_dir)
 
 for path in paths:
-    cap = cv2.VideoCapture(path)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frames = 30
-    start_time = time.time()
-
-    screen_size = (int(width * 0.5), int(height * 0.5))
-    screen = pygame.display.set_mode(screen_size)
-
-    print()
-    print(f"Working on '{path}'...")
-
-    if False:
-        with open(f"{path}.data", "rb") as f:
-            data = np.load(f, allow_pickle=True).item()
-            fused_track = data["track"]
-
-        if False:
-            visualize_track(0, fused_track)
-
-            continue
+    frame = cv2.imread(path)
+    width, height, _ = frame.shape
 
 
-    for iter in range(1):
-        print(f"Working on iteration {iter}...")
-
-        contours_enum = do_dense_segmentation(cap, frames)
-        fused_track = do_fused_track(cap, contours_enum)
-
-        # Guided dense segmentation
-        contours = []
-        for frame_index in tqdm(range(len(fused_track)), desc="Guided dense segmentation"):
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-            frame = read_frame(cap)
-
-            corners = []
-            for box in fused_track[frame_index]: 
-                x, y, w, h = box
-                x, y, w, h = int(x), int(y), int(w), int(h)
-                corners.append((x + w / 2.0, y + h / 2.0))
-
-            corners = np.array(corners)
-            
-            middle = corners.mean(0)[np.newaxis, :]
-            middle_corners = corners - middle
-            small_contour = middle + middle_corners * mask_size_factors[0]
-            large_contour = middle + middle_corners * mask_size_factors[1]
+    # contours_enum = do_dense_segmentation(cap, frames)
+    # fused_track = do_fused_track(cap, contours_enum)
 
 
-            pr_fg_mask = get_mask_from_contour(corners, frame.shape[0:2])
-            fg_mask = get_mask_from_contour(small_contour, frame.shape[0:2])
-            bg_mask = get_mask_from_contour(large_contour, frame.shape[0:2])  
-            bg_mask = cv2.bitwise_not(bg_mask)
 
-
-            # unshapen mask frame
-            frame = unsharpen_mask(frame, sigma=4.0, alpha=2.0)
-
-
-            # grabcut the fine contours
-            contour = grab_cut(frame, pr_fg_mask, fg_mask, bg_mask, frame.shape[0:2])
-            contour = get_simple_contour(contour, 0.001)
-
-            contours.append(contour)
-
-
-    # save to disk
-    data = {
-        "track": fused_track,
-        "contours": contours
-    }
-
-    with open(f"{path}.data", "wb") as f:
-        np.save(f, data, allow_pickle=True)
-
-
-    runtime = time.time() - start_time
-    runtime /= 60.0
-    print(f"Finished in {runtime:.2f}min")
-
-    continue
-
-
-    print("Visualizing forward tracks...")
-
-    for (_, frame_index, _), fw_track, _ in tracks:
-        visualize_track(frame_index, fw_track)
-
-    print("Visualizing backward tracks...")
-
-    for (last_frame_index, _, _), _, bw_track in tracks:
-        visualize_track(last_frame_index, bw_track)
-
-
-    cap.release()
+runtime = time.time() - start_time
+runtime /= 60.0
+print(f"Finished in {runtime:.2f}min")
--- autolabel/mesher.py
+++ autolabel/mesher.py
@@ -2,7 +2,11 @@
 import os
 import subprocess
 import sys
+import cv2
+import numpy as np
 from tqdm import tqdm
+import shutil
+from pathlib import Path
 
 # Based on:
 # https://github.com/cdcseacave/openMVS/blob/master/MvgMvsPipeline.py
@@ -13,31 +17,7 @@ OPENMVS_BIN = OPENMVG_BIN
 CAMERA_SENSOR_DB_FILE = "sensor_width_camera_database.txt"
 CAMERA_SENSOR_DB_DIRECTORY = "/home/erik/Downloads/ReleaseV2.0.Rainbow-Trout.WindowsBinaries_VS2019/exif/sensor_width_database/"
 
-class AStep:
-    """ Represents a process step to be run """
-    def __init__(self, info, cmd, opt):
-        self.info = info
-        self.cmd = cmd
-        self.opt = opt
-
-
-input_dir = "/home/erik/Downloads/ReleaseV2.0.Rainbow-Trout.WindowsBinaries_VS2019/ImageDataset_SceauxCastle/images"
-output_dir = "/home/erik/Downloads/ReleaseV2.0.Rainbow-Trout.WindowsBinaries_VS2019/ImageDataset_SceauxCastle/output"
-reconstruction_dir = "/home/erik/Downloads/ReleaseV2.0.Rainbow-Trout.WindowsBinaries_VS2019/ImageDataset_SceauxCastle/reconstruction"
-
-if not os.path.exists(input_dir):
-    sys.exit(f"input path not found: {input_dir}")
-
-
-def replace_opt(self, idx, str_exist, str_new):
-    s = self.steps_data[idx]
-    o2 = []
-    for o in s[2]:
-        co = o.replace(str_exist, str_new)
-        o2.append(co)
-    s[2] = o2
-
-def make_steps():
+def make_steps(input_dir, matches_dir, camera_file_params, reconstruction_dir, mvs_dir):
     return [
         ["Intrinsics analysis",
          os.path.join(OPENMVG_BIN, "openMVG_main_SfMInit_ImageListing"),
@@ -57,6 +37,9 @@ def make_steps():
         ["Incremental reconstruction",
          os.path.join(OPENMVG_BIN, "openMVG_main_SfM"),
          ["-i", f"{matches_dir}/sfm_data.json", "-m", matches_dir, "-o", reconstruction_dir, "-s", "INCREMENTAL"]],
+        ["Convert to json",
+         os.path.join(OPENMVG_BIN, "openMVG_main_ConvertSfM_DataFormat"),
+         ["-i", f"{reconstruction_dir}/sfm_data.bin", "-o", f"{reconstruction_dir}/sfm_data.json", "-V", "-I", "-E", "-C"]],
         ["Export to openMVS",
          os.path.join(OPENMVG_BIN, "openMVG_main_openMVG2openMVS"),
          ["-i", f"{reconstruction_dir}/sfm_data.bin", "-o", f"{mvs_dir}/scene.mvs", "-d", f"{mvs_dir}/images"]],
@@ -77,19 +60,27 @@ def make_dirs(dirs):
         if not os.path.exists(dir):
             os.mkdir(dir)
 
+def rm_dirs(dirs):
+    for dir in dirs:
+        shutil.rmtree(dir, ignore_errors=True)
 
-reconstruction_dir, matches_dir = os.path.join(output_dir, "sfm"), os.path.join(reconstruction_dir, "matches") 
-mvs_dir = os.path.join(output_dir, "mvs")
-camera_file_params = os.path.join(CAMERA_SENSOR_DB_DIRECTORY, CAMERA_SENSOR_DB_FILE)
+def solve_in_3d(input_dir, output_dir):
+    input_dir = Path(input_dir).absolute()
+    output_dir = Path(output_dir).absolute()
 
-make_dirs([output_dir, reconstruction_dir, matches_dir, mvs_dir])
+    if not os.path.exists(input_dir):
+        sys.exit(f"input path not found: {input_dir}")
 
-steps = make_steps()
+    rm_dirs([output_dir])
 
-# RefineMesh step is not run, use ReconstructMesh output
-# steps.replace_opt(15, "scene_dense_mesh_refine.mvs", "scene_dense_mesh.mvs")
+    reconstruction_dir, matches_dir = os.path.join(output_dir, "sfm"), os.path.join(output_dir, "matches") 
+    mvs_dir = os.path.join(output_dir, "mvs")
+    camera_file_params = os.path.join(CAMERA_SENSOR_DB_DIRECTORY, CAMERA_SENSOR_DB_FILE)
+    make_dirs([output_dir, matches_dir, reconstruction_dir, mvs_dir])
 
-def solve_in_3d():
+    steps = make_steps(input_dir, matches_dir, camera_file_params, reconstruction_dir, mvs_dir)
+
+
     pbar = tqdm(total=len(steps))
 
     for [name, path, args] in steps:
@@ -99,10 +90,13 @@ def solve_in_3d():
             cmdline = [path] + args
             pipes = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             std_out, std_err = pipes.communicate()
+            pipes.wait()
             
-            if pipes.returncode != 0:
-                err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
-                print(err_msg)
+            if pipes.returncode != 0 and std_err != b"":
+                print("--- STANDARD OUTPUT ---")
+                print(std_out.decode())
+                print("--- STANDARD ERROR ---")
+                print(std_err.decode())
                 exit()
 
         except KeyboardInterrupt:
@@ -112,4 +106,119 @@ def solve_in_3d():
 
     pbar.close()
 
-solve_in_3d()
+
+import open3d as o3d
+import open3d.core as o3c
+import matplotlib.pyplot as plt
+import json
+
+def test(input_dir, output_dir):
+    sfm_data_path = f"{output_dir}/sfm/sfm_data.json"
+    scene_dense_path = f"{output_dir}/mvs/scene_dense_mesh.ply"
+
+    with open(sfm_data_path, "r") as f:
+        data = json.load(f)
+
+    views = []
+    intrinsics, extrinsics = {}, {}
+
+
+    for extrinsic in data["extrinsics"]:
+        key = extrinsic["key"]
+
+        view = extrinsic["value"]
+
+        rotation = np.array(view["rotation"]).reshape((3, 3))
+        rotation = np.linalg.inv(rotation)
+
+        extrinsic_matrix = np.eye(4)
+        extrinsic_matrix[:3,:3] = rotation
+        extrinsic_matrix[:3,3] = np.array(view["center"])
+
+        extrinsic_matrix = np.linalg.inv(extrinsic_matrix)
+        extrinsics[key] = extrinsic_matrix
+
+
+    if False:
+        # Load the sfm_data.json file
+        sfm_data_path = f"{output_dir}/sfm/sfm_data.json"
+        with open(sfm_data_path, "r") as f:
+            sfm_data = json.load(f)
+
+        # Create an Open3D visualization window
+        vis = o3d.visualization.Visualizer()
+        vis.create_window()
+
+        # Add the dense point cloud to the visualization
+        pcd = o3d.io.read_point_cloud(f"{output_dir}/mvs/scene_dense.ply")
+        vis.add_geometry(pcd)
+
+        pcd = o3d.io.read_triangle_mesh(scene_dense_path)
+        vis.add_geometry(pcd)
+
+        # Add the camera poses to the visualization
+        for view in sfm_data["views"]:
+            pose_id = view["value"]["ptr_wrapper"]["data"]["id_pose"]
+            extrinsic = extrinsics[pose_id]
+            
+            camera = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
+            camera.transform(extrinsic)
+            vis.add_geometry(camera)
+
+        # Run the visualization
+        vis.run()
+        vis.destroy_window()
+        exit()
+
+    for view in data["views"]:
+        view = view["value"]["ptr_wrapper"]["data"]
+        filename = view["filename"]
+        size = (view["width"], view["height"])
+        id_pose = view["id_pose"]
+        id_intrinsic = view["id_intrinsic"]
+
+        views.append((filename, size, id_pose, id_intrinsic))
+
+
+    for intrinsic in data["intrinsics"]:
+        key = intrinsic["key"]
+
+        view = intrinsic["value"]["ptr_wrapper"]["data"]
+        size = (view["width"], view["height"])
+        focal_length = view["focal_length"]
+        principal_point = view["principal_point"]
+        distortion_coeff_k3 = view["disto_k3"]        
+
+        intrinsic_matrix = np.array([
+            [focal_length, 0, principal_point[0]],
+            [0, focal_length, principal_point[1]],
+            [0, 0, 1]
+        ], np.float32)
+
+        intrinsics[key] = (size, intrinsic_matrix, distortion_coeff_k3)
+
+
+    mesh = o3d.io.read_triangle_mesh(scene_dense_path)
+    mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
+    
+    scene = o3d.t.geometry.RaycastingScene()
+    scene.add_triangles(mesh)
+
+    for i in range(len(views)):
+        filename, size, pose_id, id_intrinsic = views[i]
+        size, intrinsic_matrix, distortion_coeff_k3 = intrinsics[id_intrinsic]
+        extrinsic_matrix = extrinsics[pose_id]
+        
+        intrinsic_matrix = o3c.Tensor(intrinsic_matrix)
+        extrinsic_matrix = o3c.Tensor(extrinsic_matrix) 
+
+        rays = scene.create_rays_pinhole(intrinsic_matrix, extrinsic_matrix, size[0], size[1])
+        ans = scene.cast_rays(rays)
+
+        depth = np.rot90(ans['t_hit'].numpy(), k=3)[:, :, np.newaxis]
+        img = cv2.imread(f"{input_dir}/{filename}")
+        img = img.astype("float32") / 255.0
+
+        plt.imshow(depth)
+        plt.title(filename)        
+        plt.show()
